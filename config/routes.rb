ee6e5910e55026755707d4eaeb63fdc88e678d04
rafaeldahis <rdahis@twitter.com>
2016-02-11 02:56:10 -0800

    fixing some edge cases, like wrong names or no names at all

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,16 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'cities#index'
+  root 'pages#show'
 
 #api
   namespace :api do
-    resources :cities
+    resources :cities, only: [:show]
   end
   
 get 'cities/:name' => 'pages#show'
 
-resources :cities
-  
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
